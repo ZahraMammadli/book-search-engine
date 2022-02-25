e1d1947740910bd07175a19ad8833fe3a30cf470
Zahra Mammadli <zahra.mrj@gmail.com>
2022-02-25 19:25:41 +0100

    Initial commit

---
+++ Develop/server/schemas/typeDefs.js
@@ -0,0 +1,15 @@
+const typeDefs = gql`
+  type Query {
+ 
+  }
+ 
+
+  type Mutation {
+    addProfile(name: String!): Profile
+    addSkill(profileId: ID!, skill: String!): Profile
+    removeProfile(profileId: ID!): Profile
+    removeSkill(profileId: ID!, skill: String!): Profile
+  }
+`;
+
+module.exports = typeDefs;

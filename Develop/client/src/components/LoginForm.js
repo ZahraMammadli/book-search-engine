d9758d5ef07bbf81b52d9927a21a4d33f6a89207
Zahra Mammadli <zahra.mrj@gmail.com>
2022-03-05 17:39:31 +0100

    Finish sign up react part with graph ql

--- Develop/client/src/components/LoginForm.js
+++ Develop/client/src/components/LoginForm.js
@@ -1,8 +1,6 @@
 // see SignupForm.js for comments
 import React, { useState } from "react";
 import { Form, Button, Alert } from "react-bootstrap";
-
-// import { loginUser } from "../utils/API";
 import Auth from "../utils/auth";
 
 import { LOGIN_USER } from "../utils/mutations";
@@ -39,20 +37,6 @@ const LoginForm = () => {
       console.error(e);
       setShowAlert(true);
     }
-    // try {
-    //   const response = await loginUser(userFormData);
-
-    //   if (!response.ok) {
-    //     throw new Error("something went wrong!");
-    //   }
-
-    //   const { token, user } = await response.json();
-    //   console.log(user);
-    //   Auth.login(token);
-    // } catch (err) {
-    //   console.error(err);
-    //   setShowAlert(true);
-    // }
 
     setUserFormData({
       username: "",
